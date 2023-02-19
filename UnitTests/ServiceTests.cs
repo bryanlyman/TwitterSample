a697deacfe73d1a51be3a2368b4ea12d230e40ee
Bryan Lyman <bryanlyman1@hotmail.com>
2023-02-19 02:26:55 -0700

    Added pie chart

--- UnitTests/ServiceTests.cs
+++ UnitTests/ServiceTests.cs
@@ -14,5 +14,24 @@ namespace UnitTests
         {
             Assert.AreEqual(ServiceFactory.GetService(eServiceType.Reddit).GetType(), typeof(RedditService));
         }
+
+        [TestMethod]
+        public void RedditService_GetSampleSocketURL_HTTPS()
+        {
+            var uri = new Uri("https://127.0.0.1:3030");
+            var redditService = ServiceFactory.GetService(eServiceType.Reddit);
+            var socketUrl = redditService.GetSampleSocketURL(uri);
+            Assert.AreEqual(socketUrl, "wss://127.0.0.1:3030/redditstream");
+        }
+
+        [TestMethod]
+        public void RedditService_GetSampleSocketURL_HTTP()
+        {
+            var uri = new Uri("http://127.0.0.1:3030");
+            var redditService = ServiceFactory.GetService(eServiceType.Reddit);
+            var socketUrl = redditService.GetSampleSocketURL(uri);
+            Assert.AreEqual(socketUrl, "ws://127.0.0.1:3030/redditstream");
+        }
+
     }
 }
